46162b9f10498c4fb525d9011e8d65cf1fe1ab23
fredkk22 <freddykwak123@gmail.com>
2022-09-13 14:34:33 -0400

    Added to home computer from laptop

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true, required: true, trimmed: true },
+  email: { type: String, unique: false, required: true, match: /.+\@.+\..+/ },
+  thoughts: [Thought],
+  friends: [User]
+});
+
+// Using mongoose.model() to compile a model based on the schema 'bookSchema'
+const User = mongoose.model('User', userSchema);
+
+// Create new instances of the model, a document
+User.create([
+  { username: 'bob33', email: 'bob33@gmail.com' },
+  { username: 'bob44', email: 'bob44@gmail.com' },
+  { username: 'theo49', email: 'theo@gmail.com' },
+  { username: 'paul402', email: 'paul@gmail.com' },
+  { username: 'fred22', email: 'fred@gmail.com' },
+  { username: 'young04', email: 'young@gmail.com' },
+  { username: 'rhett39', email: 'rhett@gmail.com' }
+]);
+
+module.exports = User;
