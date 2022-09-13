76f14e6bb711e09c2539591ee4300b62fefa4c09
fredkk22 <freddykwak123@gmail.com>
2022-09-13 16:09:01 -0400

    Removed get: formatDate from reaction schema and thought model

--- models/Thought.js
+++ models/Thought.js
@@ -4,7 +4,7 @@ const reactionSchema = require('./Reaction');
 const thoughtSchema = new Schema(
     {
         thoughtText: { type: String, required: true, minlength: 1, maxlength: 280 },
-        createdAt: { type: Date, default: Date.now, get: formatDate },
+        createdAt: { type: Date, default: Date.now },
         username: { type: String, required: true },
         reactions: [reactionSchema]
     },
